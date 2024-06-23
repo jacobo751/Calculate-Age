54f27c24123cf5491654d2774534fa7966f08684
Jacobo Vazquez <vazquezmjacobo@gmail.com>
2024-06-23 16:43:07 -0400

    New project calculate age

--- Calculate Age/main.c
+++ Calculate Age/main.c
@@ -7,8 +7,24 @@
 
 #include <stdio.h>
 
+/* function declaration */
+int calculateAge(int age);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    /* local variables definition */
+    int age = 40;
+    int ret;
+    
+    /* calling the calculateAge function to get final age */
+    ret = calculateAge(age);
+    
+    printf("Age: %d", ret);
+    printf("\n");
+    
     return 0;
+}
+
+/* function to calculate the final age of a given age */
+int calculateAge(int age){
+    return age * 1.2;
 }
