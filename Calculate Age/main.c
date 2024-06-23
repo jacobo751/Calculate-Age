f30f47a973743642a7aa52b6c3bbfb2cf9e9084f
Jacobo Vazquez <vazquezmjacobo@gmail.com>
2024-06-23 16:29:48 -0400

    Initial Commit

---
+++ Calculate Age/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Calculate Age
+//
+//  Created by Jacobo Vazquez on 6/23/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
